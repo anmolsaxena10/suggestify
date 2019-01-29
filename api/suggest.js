e331b32e2d4de72b4e6d25c16ff70fff3940ab4e
anmolsaxena10 <anmol.saxena10@gmail.com>
2019-01-30 01:19:16 +0530

    saved the code from call back hellgit add .!

--- api/suggest.js
+++ api/suggest.js
@@ -1,15 +1,28 @@
-var helper = require('../lib/redis_helper');
+var redisHelper = require('../lib/redis_helper');
+var cassandraHelper = require('../lib/cassandra_helper');
 
-module.exports = function(req, res){
-    helper.fetchSuggestions(req.params.query).then(
-		suggestions => {
-			if (suggestions != undefined) {
-				res.json(suggestions);
-			}
-		},
-		err => {
-            console.log(err);
-            res.json([]);
-		}
-	);
+module.exports = async function(req, res){
+    // redisHelper.fetchSuggestions(req.params.query).then(
+	// 	suggestions => {
+	// 		if (suggestions != undefined) {
+	// 			res.json(suggestions);
+	// 		}
+	// 	},
+	// 	err => {
+    //         console.log(err);
+    //         cassandraHelper.updateRedis(req.params.query);
+
+    //         res.json([]);
+	// 	}
+    // );
+    
+    try{
+        res.json(await redisHelper.fetchSuggestions(req.params.query));
+    }
+    catch(err){
+        await cassandraHelper.updateRedis(req.params.query);
+        res.json(await redisHelper.fetchSuggestions(req.params.query));
+    }
+
+    
 };
